f0ab07c3cd1f5705b7ef96cffe1003bb4c6311a4
Sveta616 <152242882+Sveta616@users.noreply.github.com>
2024-09-06 20:49:30 +0500

    Практическая 20 SnackBar Маринина ПР-31

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Marinina_pract20_Snackbar_PR31"
+include(":app")
+ 
